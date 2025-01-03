4fc5cbf3dbfa07549247b70e8b30c7eaa6205ba2
Petar <petar.filipov12@gmail.com>
2025-01-03 16:43:23 +0200

    Set Spi mode to slave

--- AVR_Proj/AVR_Proj_Slave/GUI_Generator_AVR_Proj_Slave/Generator/GenData/GEN_SPI_Service_CFG.h
+++ AVR_Proj/AVR_Proj_Slave/GUI_Generator_AVR_Proj_Slave/Generator/GenData/GEN_SPI_Service_CFG.h
@@ -6,7 +6,7 @@
 #define SPI_Service_SPI_Service_SPIs_Count 1
 
 #define SPI_Service_SPI_0_Id 0
-#define SPI_Service_SPI_0_SpiMode SPI_MODE_MASTER
+#define SPI_Service_SPI_0_SpiMode SPI_MODE_SLAVE
 #define SPI_Service_SPI_0_SpiRxBufferSize 10
 
 #endif
