253fb2f5253bd8b55506ae54260015c7643d48c3
Petar <petar.filipov12@gmail.com>
2025-01-03 15:31:15 +0200

    Fixed Gui Generation to allways generate data cfg files

---
+++ AVR_Proj/AVR_Proj_Slave/GUI_Generator_AVR_Proj_Slave/Generator/GenData/GEN_DIO_Service_DATA_CFG.c
@@ -0,0 +1,5 @@
+#include "GEN_DIO_Service_DATA_CFG.h"
+
+inline void DIO_Service_PinCfg_data_INIT_FUNC(void)
+{
+}
---
+++ AVR_Proj/AVR_Proj_Slave/GUI_Generator_AVR_Proj_Slave/Generator/GenData/GEN_DIO_Service_DATA_CFG.h
@@ -0,0 +1,11 @@
+#ifndef GEN_DIO_SERVICE_DATA_CFG_H
+#define GEN_DIO_SERVICE_DATA_CFG_H
+
+#include "Types.h"
+#include "Queue.h"
+#include "GEN_DIO_Service_CFG.h"
+#include "DIO_Service.h"
+
+extern inline void DIO_Service_PinCfg_data_INIT_FUNC(void);
+
+#endif
