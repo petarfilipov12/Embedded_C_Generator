035ebcd53fcb350d769c27a22433149700962ab0
petarfilipov12 <petar.filipov12@gmail.com>
2025-01-07 16:05:43 +0200

    make clean slave

--- AVR_Proj/AVR_Proj_Slave/build/HAL/ADC/ADC.d
+++
@@ -1,6 +0,0 @@
-build/HAL/ADC/ADC.o: code/HAL/ADC/ADC.c code/HAL/ADC/ADC.h \
- code/common/Types.h
-
-code/HAL/ADC/ADC.h:
-
-code/common/Types.h:
--- AVR_Proj/AVR_Proj_Slave/build/OS/OsHAL/OS_HAL_Interrupt.d
+++
@@ -1,4 +0,0 @@
-build/OS/OsHAL/OS_HAL_Interrupt.o: code/OS/OsHAL/OS_HAL_Interrupt.c \
- code/OS/OsHAL/OS_HAL_Interrupt.h
-
-code/OS/OsHAL/OS_HAL_Interrupt.h:
