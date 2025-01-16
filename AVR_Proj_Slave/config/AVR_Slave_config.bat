e774a6a1419f170b1602ca120b24e21e90655798
Petar <petar.filipov12@gmail.com>
2025-01-16 21:19:28 +0200

    Changed gen location

--- AVR_Proj_Slave/config/AVR_Slave_config.bat
+++ AVR_Proj_Slave/config/AVR_Slave_config.bat
@@ -1,3 +1,3 @@
 @echo off
 
-start ..\..\GUI_Generator\dist\main.exe --proj_title AVR_Slave --data_dir ./data --gen_dir ./gen
+start ..\..\GUI_Generator\dist\main.exe --proj_title AVR_Slave --data_dir ./data --gen_dir ../code/GenData
